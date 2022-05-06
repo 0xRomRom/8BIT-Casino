202c8078c0725f700323a58a4feeb4143357c795
0xromrom <roman0811@live.nl>
2022-05-05 17:12:47 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const playerBalance = document.querySelector(".player-funds");
+const gameBalance = document.querySelector(".game-balance");
+const slotsTitle = document.querySelector(".slots-title");
+const insertCredits = document.querySelector(".insert-credits");
+const playButton = document.querySelector(".play-button");
+const insertButton = document.querySelector(".insert-button");
+const collectButton = document.querySelector(".collect-button");
+
+//Logic
+let funds = 100;
+let inGameBalance = 0;
+
+//
+playerBalance.textContent = funds;
+insertButton.addEventListener("click", () => {
+  if (funds >= 10) {
+    funds -= 10;
+    inGameBalance += 10;
+  }
+  playerBalance.textContent = funds;
+  gameBalance.textContent = inGameBalance;
+});
+
+//
+gameBalance.textContent = inGameBalance;
+collectButton.addEventListener("click", () => {
+  funds += inGameBalance;
+  inGameBalance = 0;
+  gameBalance.textContent = inGameBalance;
+  playerBalance.textContent = funds;
+});
+
+playButton.addEventListener("click", () => {
+  if (inGameBalance >= 1) {
+    inGameBalance -= 1;
+    insertCredits.classList.add("hidden");
+  }
+
+  gameBalance.textContent = inGameBalance;
+});

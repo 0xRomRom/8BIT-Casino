5d6cbf8cb3378ee40fd37f8191638efe6355a9da
0xromrom <roman0811@live.nl>
2022-05-06 06:32:05 -0700

    Basic functionality working

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const insertCredits = document.querySelector(".insert-credits");
 const playButton = document.querySelector(".play-button");
 const insertButton = document.querySelector(".insert-button");
 const collectButton = document.querySelector(".collect-button");
+const slot1 = document.querySelector(".slot-1");
+const slot2 = document.querySelector(".slot-2");
+const slot3 = document.querySelector(".slot-3");
 
 //Logic
 let funds = 100;
@@ -15,6 +18,20 @@ let inGameBalance = 0;
 //
 playerBalance.textContent = funds;
 insertButton.addEventListener("click", () => {
+  insertFunds();
+});
+
+//
+gameBalance.textContent = inGameBalance;
+collectButton.addEventListener("click", () => {
+  collectFunds();
+});
+
+playButton.addEventListener("click", () => {
+  playGame();
+});
+
+const insertFunds = () => {
   if (funds >= 10) {
     funds -= 10;
     inGameBalance += 10;
@@ -27,22 +44,32 @@ insertButton.addEventListener("click", () => {
   }
   playerBalance.textContent = funds;
   gameBalance.textContent = inGameBalance;
-});
+};
 
-//
-gameBalance.textContent = inGameBalance;
-collectButton.addEventListener("click", () => {
+const collectFunds = () => {
   funds += inGameBalance;
   inGameBalance = 0;
   gameBalance.textContent = inGameBalance;
   playerBalance.textContent = funds;
-});
+};
 
-playButton.addEventListener("click", () => {
+const playGame = () => {
   if (inGameBalance >= 1) {
     inGameBalance -= 1;
     insertCredits.classList.add("hidden");
+    randomSpins();
   }
-
   gameBalance.textContent = inGameBalance;
-});
+};
+
+const randomSpins = () => {
+  const randomInt1 = Math.trunc(Math.random() * 16 + 1);
+  const randomInt2 = Math.trunc(Math.random() * 16 + 1);
+  const randomInt3 = Math.trunc(Math.random() * 16 + 1);
+  console.log(randomInt1);
+  console.log(randomInt2);
+  console.log(randomInt3);
+  slot1.src = `icons/${randomInt1}.png`;
+  slot2.src = `icons/${randomInt2}.png`;
+  slot3.src = `icons/${randomInt3}.png`;
+};
